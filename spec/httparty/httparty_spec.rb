2de3e2556252a411bb545a9410145ccd87d261b2
guimelo03 <guilhermemelo03@hotmail.com>
2025-09-30 16:54:18 -0300

    configurando os cassettes para salvar dados da api

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,7 +1,7 @@
 describe "HTTParty" do
-    it 'content-type' do
-        stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2').
-            to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
+    it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts' } do
+        # stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2').
+        #     to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
 
         response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
         content_type = response.headers['content-type']
