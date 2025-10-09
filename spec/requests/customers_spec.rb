e07bd1a448cce0fc2d7c0fbb318324304de7bba0
Guimelo03 <guilhermemelo03@hotmail.com>
2025-10-09 15:46:45 -0300

    testes com requisições(requests)

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -17,6 +17,17 @@ RSpec.describe "/customers", type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Customer. As you add validations to Customer, be sure to
   # adjust the attributes here as well.
+
+  it 'GET responds successfully' do
+    get customers_path
+    expect(response).to be_successful
+  end
+
+  it 'GET responds a 200 response' do
+    get customers_path
+    expect(response).to have_http_status(200)
+  end
+
   let(:valid_attributes) {
     skip("Add a hash of attributes valid for your model")
   }
