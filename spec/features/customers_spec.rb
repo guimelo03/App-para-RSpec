3100dab8cfb81d75e87c28edaf2db4a126754187
Guimelo03 <guilhermemelo03@hotmail.com>
2025-10-15 07:05:52 -0300

    testando views com capybara

---
+++ spec/features/customers_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.feature "Customers", type: :feature, js: true do
+  it 'Visit index page' do
+    visit(customers_path)
+    expect(page).to have_current_path(customers_path)
+  end
+end
