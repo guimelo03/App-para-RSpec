303a2fb514a23d0eae1ca265a6dc4d511e4d976e
Guimelo03 <guilhermemelo03@hotmail.com>
2025-10-15 17:51:20 -0300

    finalizando testes com views, utilizando page object pattern

--- spec/features/customers_spec.rb
+++ spec/features/customers_spec.rb
@@ -1,10 +1,34 @@
 require 'rails_helper'
+require_relative '../support/new_customer_form'
 
 RSpec.feature "Customers", type: :feature, js: true do
   it 'Visit index page' do
     visit(customers_path)
     page.save_screenshot('screenshot.png')
     expect(page).to have_current_path(customers_path)
+  end
+
+  it 'Ajax' do
+    visit(customers_path)
+    click_link('Add Message')
+    expect(page).to have_content('Yes!', wait: 5)
+  end
+
+  it 'Find' do
+    visit(customers_path)
+    click_link('Add Message')
+    expect(find('#my-div', wait: 5).find('h1')).to have_content('Yes!', wait: 5)
+  end
+
+  it 'Creates a Customer - Page Object Pattern' do
+    new_customer_form = NewCustomerForm.new
+    new_customer_form.login.visit_page.fill_in_with(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      address: Faker::Address.street_address
+    ).submit
+
+    expect(page).to have_content('Customer was successfully created.')
   end
 
   it 'Creates a Customer' do
