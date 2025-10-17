9fdb06c60ab5695c8b462c86bdb94fafd9974609
Guimelo03 <guilhermemelo03@hotmail.com>
2025-10-17 08:40:21 -0300

    teste com RSpec puro e customers.json

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -159,6 +159,14 @@ RSpec.describe "/customers", type: :request do
       )
     end
 
+    it 'show - JSON com RSpec puro' do
+      get "/customers/1.json"
+      response_body = JSON.parse(response.body)
+      expect(response_body.fetch("id")).to eq(1)
+      expect(response_body.fetch("name")).to be_kind_of(String)
+      expect(response_body.fetch("email")).to be_kind_of(String)
+    end
+
     it 'create - JSON' do
       member = create(:member)
       login_as(member, scope: :member)
